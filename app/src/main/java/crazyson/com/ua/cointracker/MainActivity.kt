ddaae36355e7e9f5c803ca9d76b5b2192c67619b
Vlad <lubinvlad@icloud.com>
2022-05-04 13:33:11 +0300

    finished app

--- app/src/main/java/crazyson/com/ua/cointracker/MainActivity.kt
+++ app/src/main/java/crazyson/com/ua/cointracker/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.ramcosta.composedestinations.DestinationsNavHost
-import crazyson.com.ua.cointracker.presentation.company_listings.NavGraphs
+import crazyson.com.ua.cointracker.presentation.NavGraphs
 import crazyson.com.ua.cointracker.ui.theme.CoinTrackerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
